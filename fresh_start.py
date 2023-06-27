72d40a51f7992acfa64c41c6b9f521e9bb58c22c
Zhanat Darmenov <Zhandarm2000@gmail.com>
2023-06-27 20:24:00 +0300

    main

--- fresh_start.py
+++ fresh_start.py
@@ -1,2 +1,17 @@
+from fresh_start_1 import *
 
-print("Here we go again.")
+
+def game_cycle():
+    game_on = True
+    name1 = ''
+    name2 = ''
+    get_players(name1, name2)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    game_cycle()
--- fresh_start_1.py
+++ fresh_start_1.py
@@ -4,6 +4,4 @@ def get_players(name1, name2):
     name2=input("Player 2, enter your name: ")
     print("Hi ", name2)
 
-name1 = ''
-name2= ''
-get_players(name1, name2)
+
