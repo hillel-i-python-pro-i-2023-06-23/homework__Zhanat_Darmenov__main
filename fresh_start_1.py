c5f49c1f4198a2913dbb0e65060904ccfc01dafa
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-06-27 19:36:55 +0200

    ver 3

--- fresh_start_1.py
+++ fresh_start_1.py
@@ -4,4 +4,23 @@ def get_players(name1, name2):
     name2=input("Player 2, enter your name: ")
     print("Hi ", name2)
 
+def input_word(name_player, word_list):
+    word = input('Your turn, player '+name_player+' enter your word:')
+    if not word in word_list:
+        word_list.append(word)
 
+
+def check_get_players(name1, name2):
+
+    get_players(name1, name2)
+    print('name of player 1', name1)
+    print('name of player 2', name2)
+
+if __name__=="__main__":
+    name1 = ''
+    name2 = ''
+    wordlist1 = []
+    wordlist2 = []
+    check_get_players(name1, name2)
+    input_word(name1, wordlist1)
+    input_word(name2, wordlist2)
