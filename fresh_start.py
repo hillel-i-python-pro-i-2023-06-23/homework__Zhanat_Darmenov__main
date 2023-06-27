bb368a54b9f1ae0974a7dca96a7034487986ce17
Zhanat Darmenov <Zhandarm2000@gmail.com>
2023-06-27 20:48:11 +0300

    main4

--- fresh_start.py
+++ fresh_start.py
@@ -10,18 +10,21 @@ def game_cycle():
     name2 = ''
     get_players(name1, name2)
 
+    wordlist1 = []
+    wordlist2 = []
+
     while(game_on):
         # Add Player word to List:
         if player1:
             # Add first player word to List
-            input_word(name1, wordlist1)
+            input_word(name1, wordlist1, game_on)
             player1 = False
         else:
             # Add second player word to List
-            input_word(name2, wordlist2)
+            input_word(name2, wordlist2, game_on)
             player1 = True
 
-        
+
 
 
 
