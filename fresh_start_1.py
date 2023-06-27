817bbe946d170d979c85e052c7c32bc0ed090ea6
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-06-27 19:14:46 +0200

    ver 2

--- fresh_start_1.py
+++ fresh_start_1.py
@@ -1,4 +1,9 @@
 def get_players(name1, name2):
-    name1=input("Ваше имя: ")
+    name1=input("Player 1, enter your name: ")
+    print("Hi ", name1)
+    name2=input("Player 2, enter your name: ")
+    print("Hi ", name2)
 
-    name2=input("Ваше имя: "))
+name1 = ''
+name2= ''
+get_players(name1, name2)
